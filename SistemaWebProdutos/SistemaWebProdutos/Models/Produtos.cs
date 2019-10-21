ffd1d95e9654343223beeb912e7be7686ac6f752
LucasZubu <56839721+LucasZubu@users.noreply.github.com>
2019-10-21 14:30:26 -0300

    Cadastro de Produtos
    Cadastre produtos variados.

---
+++ SistemaWebProdutos/SistemaWebProdutos/Models/Produtos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaWebProdutos.Models
+{
+    public partial class Produtos
+    {
+        public int ProdCod { get; set; }
+        public string ProdNome { get; set; }
+        public decimal ProdPreco { get; set; }
+        public int ProdQntd { get; set; }
+    }
+}
---
+++ SistemaWebProdutos/SistemaWebProdutos/Models/sistemaweb21Context.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SistemaWebProdutos.Models
+{
+    public partial class sistemaweb21Context : DbContext
+    {
+        public sistemaweb21Context()
+        {
+        }
+
+        public sistemaweb21Context(DbContextOptions<sistemaweb21Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Produtos> Produtos { get; set; }
+
+      
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produtos>(entity =>
+            {
+                entity.HasKey(e => e.ProdCod);
+
+                entity.Property(e => e.ProdCod).HasColumnName("Prod_Cod");
+
+                entity.Property(e => e.ProdNome)
+                    .IsRequired()
+                    .HasColumnName("Prod_Nome")
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.ProdPreco)
+                    .HasColumnName("Prod_Preco")
+                    .HasColumnType("decimal(14, 2)");
+
+                entity.Property(e => e.ProdQntd).HasColumnName("Prod_Qntd");
+            });
+        }
+    }
+}
